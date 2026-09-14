b27649e29f487c20a532b97b4bd44c17b477e361
Diana K <dkavelic77@gmail.com>
2026-09-14 16:22:02 +0300

    изменили  текст для главного окна

--- лаба 2/lab2/Form1.Designer.cs
+++ лаба 2/lab2/Form1.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Name = "Form1";
+            Text = "Лаба 2. Наследование";
+            ResumeLayout(false);
         }
 
         #endregion
